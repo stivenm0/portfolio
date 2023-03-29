ca20d030bec3a3c02a4d6928eedb5ae3069cca57
stivenm0 <estivenyeizon@gmail.com>
2023-03-28 20:55:26 -0500

    finalizado

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,20 +1,26 @@
 export const proyectos= [
     {
+        image: "https://raw.githubusercontent.com/stivenm0/vault/main/web/home.png",
+        description: "Vault <span> (HTML CSS JS)</span>",
+        github: "https://github.com/stivenm0/vault",
+        demo: "https://stivenm0.github.io/vault/"
+    },
+    {
         image: "https://raw.githubusercontent.com/stivenm0/Launch_X/main/Pokedex/images/pokedex.png",
         description: " Prokedex <span> (HTML CSS JS)</span>",
-        github: "",
-        demo: ""
+        github: "https://github.com/stivenm0/Launch_X/tree/main/Pokedex",
+        demo: "https://s-pokedex.netlify.app/"
     },
     {
         image: "https://raw.githubusercontent.com/stivenm0/Rick-and-Morty/main/app.png",
         description: "Personajes de Rick And Morty <span> (REACT)</span>",
-        github: "",
-        demo: ""
+        github: "https://github.com/stivenm0/Rick-and-Morty",
+        demo: "https://rick-and-morty-stivenm0.vercel.app/"
     },
     {
         image: "https://raw.githubusercontent.com/stivenm0/Launch_X/main/Calculadora/app.png",
         description: "Calculadora <span> (REACT)</span>",
-        github: "",
-        demo: ""
+        github: "https://github.com/stivenm0/Launch_X/tree/main/Calculadora",
+        demo: "https://mcalculatorm.netlify.app/"
     }
 ];
